65702f5f04e6f1452f5ccd06958eb036c05062cd
Vincent <vincentamoguis@gmail.com>
2022-05-09 15:37:11 +0800

    fix: mvp

--- src/App.js
+++ src/App.js
@@ -4,37 +4,52 @@ import "./App.css";
 import jig from "./assets/images/jigglypuff-wink.png";
 import wrong from "./assets/images/wrong.png";
 
-function App() {
-
-	const [data, setData] = useState(0);
-    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const itemList = items.map((x) => <Lists data={data}  key={x.toString()} id={x} />);
-
-	const [random, setRandom] = useState(Math.floor(Math.random() * 10) + 1);
-	//const random = Math.floor(Math.random() * 10) + 1;
+    const random = Math.floor(Math.random() * 3) + 1;
 
 	console.log(random);
-	console.log(data);
 
-	function handleClick (id) {
-		if( random === id ) {
-			setData(1);
+
+
+function App() {
+    const [items,setItems] = useState([
+        {
+            id: 1,
+            state: false,
+        },
+        {
+            id: 2,
+            state: false,
+        },
+        {
+            id: 3,
+            state: false,
+        },
+    ]);
+
+
+
+    function Lists({ id }) {
+
+	const [state, setState] = useState("false")	
+	const message = " ";
+	
+		function handleClick() {
+			if(random === id) {
+				setState("correct");
+
+			}
+			else {
+				setState("wrong");
+			}
+			
 		}
-		console.log(id);
-		console.log(data);
-	}
-
-
-    function Lists({ data, id }) {
-
 
         return (
             <>
-                <div className="rounded-3xl m-2 hover:bg-sky-700 bg-local border-2 border-black w-24 h-24 text-4xl"
-					id={id}
-					onClick={ () => handleClick(Number(id))}
-				>
-                    {id}
+                <div
+                    className={ `${state} m-2 h-24 w-24 rounded-3xl border-2 border-black bg-local text-4xl hover:bg-sky-700` }
+                    onClick={() => handleClick()}
+                >
                 </div>
             </>
         );
@@ -43,9 +58,16 @@ function App() {
     return (
         <div>
             <div className="flex flex-col">
-                <h1 className="text-4xl text-center text-amber-50 mt-8">Guess the Pic!</h1>
+                <h1 className="mt-8 text-center text-4xl text-amber-50">
+                    Guess the Pic!
+                </h1>
                 <div className="flex flex-wrap ">
-					{itemList}
+
+				<Lists id={1} /> 
+				<Lists id={2} /> 
+				<Lists id={3} /> 
+
+
 				</div>
                 <div></div>
             </div>
